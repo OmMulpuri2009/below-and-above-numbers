8b15464d7ec2363da217874ad94aecf840b77d7c
OmMulpuri2009 <86356509+OmMulpuri2009@users.noreply.github.com>
2021-07-05 19:57:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var marks = [35,38,41,43,45,34,68,41,48,42,12];
+
+function setup() {
+  createCanvas(400,400);
+
+  for(var i = 0;i<marks.length;i++)
+{ 
+  if (marks[i]>=30)
+
+ console.log (marks[i])
+      }
+
+    }
